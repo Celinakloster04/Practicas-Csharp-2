1c8c58290f9fee440173a8b97fa9cae45412396a
Celina <celinakloster04@gmail.com>
2024-01-08 21:49:37 -0300

    Primera app en Winforms

---
+++ MiPrimerAplicacion/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace MiPrimerAplicacion
+{
+    partial class MiPrimerAplicacion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txbApellido = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbEdad = new System.Windows.Forms.TextBox();
+            this.txbDireccion = new System.Windows.Forms.TextBox();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNombre.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblNombre.Location = new System.Drawing.Point(18, 81);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(76, 22);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "NOMBRE";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblApellido.Location = new System.Drawing.Point(18, 37);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(81, 22);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "APELLIDO";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEdad.Location = new System.Drawing.Point(18, 129);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(52, 22);
+            this.lblEdad.TabIndex = 2;
+            this.lblEdad.Text = "EDAD";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDireccion.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblDireccion.Location = new System.Drawing.Point(18, 175);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(92, 22);
+            this.lblDireccion.TabIndex = 3;
+            this.lblDireccion.Text = "DIRECCIÓN";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lblResultado.Location = new System.Drawing.Point(18, 224);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(101, 22);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "RESULTADO";
+            // 
+            // txbApellido
+            // 
+            this.txbApellido.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txbApellido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbApellido.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbApellido.Location = new System.Drawing.Point(116, 36);
+            this.txbApellido.MaxLength = 20;
+            this.txbApellido.Name = "txbApellido";
+            this.txbApellido.Size = new System.Drawing.Size(416, 27);
+            this.txbApellido.TabIndex = 5;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbNombre.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNombre.Location = new System.Drawing.Point(116, 80);
+            this.txbNombre.MaxLength = 20;
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(416, 27);
+            this.txbNombre.TabIndex = 6;
+            // 
+            // txbEdad
+            // 
+            this.txbEdad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbEdad.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEdad.Location = new System.Drawing.Point(116, 124);
+            this.txbEdad.MaxLength = 2;
+            this.txbEdad.Name = "txbEdad";
+            this.txbEdad.Size = new System.Drawing.Size(103, 27);
+            this.txbEdad.TabIndex = 7;
+            this.txbEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbEdad_KeyPress);
+            // 
+            // txbDireccion
+            // 
+            this.txbDireccion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbDireccion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbDireccion.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDireccion.Location = new System.Drawing.Point(116, 170);
+            this.txbDireccion.MaxLength = 35;
+            this.txbDireccion.Name = "txbDireccion";
+            this.txbDireccion.Size = new System.Drawing.Size(416, 27);
+            this.txbDireccion.TabIndex = 8;
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbResultado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbResultado.Location = new System.Drawing.Point(22, 249);
+            this.txbResultado.Multiline = true;
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.ReadOnly = true;
+            this.txbResultado.Size = new System.Drawing.Size(510, 161);
+            this.txbResultado.TabIndex = 9;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAceptar.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SteelBlue;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAceptar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAceptar.Location = new System.Drawing.Point(135, 429);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(99, 33);
+            this.btnAceptar.TabIndex = 10;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SteelBlue;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancelar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCancelar.Location = new System.Drawing.Point(303, 429);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(99, 33);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // MiPrimerAplicacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(560, 493);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.txbDireccion);
+            this.Controls.Add(this.txbEdad);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.txbApellido);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MiPrimerAplicacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales";
+            this.Load += new System.EventHandler(this.MiPrimerAplicacion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txbApellido;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.TextBox txbEdad;
+        private System.Windows.Forms.TextBox txbDireccion;
+        private System.Windows.Forms.TextBox txbResultado;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ MiPrimerAplicacion/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerAplicacion
+{
+    public partial class MiPrimerAplicacion : Form
+    {
+        public MiPrimerAplicacion()
+        {
+            InitializeComponent();
+        }
+
+        private void MiPrimerAplicacion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txbApellido.Text == "")
+                txbApellido.BackColor = Color.Crimson;
+            else
+                txbApellido.BackColor = System.Drawing.SystemColors.Window;
+            if (txbNombre.Text == "")
+                txbNombre.BackColor = Color.Crimson;
+            else
+                txbNombre.BackColor = System.Drawing.SystemColors.Window;
+            if (txbEdad.Text == "")
+                txbEdad.BackColor = Color.Crimson;
+            else
+                txbEdad.BackColor = System.Drawing.SystemColors.Window;
+            if (txbDireccion.Text == "")
+                txbDireccion.BackColor = Color.Crimson;
+            else
+                txbDireccion.BackColor = System.Drawing.SystemColors.Window;
+
+            if (txbApellido.BackColor != Color.Crimson && txbNombre.BackColor != Color.Crimson && txbEdad.BackColor != Color.Crimson && txbDireccion.BackColor != Color.Crimson)
+            {
+                string apellido = txbApellido.Text;
+                string nombre = txbNombre.Text;
+                string edad = txbEdad.Text;
+                string direccion = txbDireccion.Text;
+
+                txbResultado.Text = "Apellido y Nombre: " + apellido + " " + nombre + "\r\n" + "Edad: " + edad + "\r\n" + "Dirección: " + direccion; ;
+            }
+            else
+                MessageBox.Show("Todos los campos deben ser llenados...");
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txbEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+                e.Handled = false;
+            else if (char.IsControl(e.KeyChar))
+                e.Handled = false;
+            else if (char.IsLetter(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
